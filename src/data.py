a8e7d51338ac86ff7c2defc51203fb59b1c84e0f
AshwiniKurady <ashwinikurady@gmail.com>
2026-02-09 15:45:35 -0800

    code refactoring

--- src/data.py
+++ src/data.py
@@ -4,6 +4,7 @@ from src.paths import RAW_DATA_DIR
 import pandas as pd
 from typing import Optional, List
 from datetime import datetime, timedelta
+from tqdm import tqdm
 
 def download_one_file_of_raw_data(year: int, month: int) -> Path:
     URL = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet'
@@ -69,3 +70,34 @@ def load_raw_data(
     rides = rides[['pickup_datetime','pickup_location_id']]
 
     return rides
+
+def add_missing_slots(agg_rides: pd.DataFrame) -> pd.DataFrame:
+    location_ids = agg_rides['pickup_location_id'].unique()
+    full_range = pd.date_range(agg_rides['pickup_hour'].min(),agg_rides['pickup_hour'].max(), freq = 'H')
+    output = pd.DataFrame()
+    for location_id in tqdm(location_ids):
+        agg_rides_i = agg_rides.loc[agg_rides.pickup_location_id == location_id, ['pickup_hour', 'rides']]
+        # quick way to add missing dates with 0 in a Series
+        agg_rides_i.set_index('pickup_hour', inplace =True)
+        agg_rides_i.index = pd.DatetimeIndex(agg_rides_i.index)
+        agg_rides_i = agg_rides_i.reindex(full_range,fill_value = 0)
+
+        #add back 'location_id' columns
+        agg_rides_i['pickup_location_id']=location_id
+
+        output = pd.concat([output, agg_rides_i])
+    
+    # #move the purchase day from the index to a dataframe column
+    output = output.reset_index().rename(columns = {'index':'pickup_hour'})
+    return output
+
+def transform_raw_data_into_ts_data(rides: pd.DataFrame) -> pd.DataFrame:
+
+    rides['pickup_hour']= rides['pickup_datetime'].dt.floor('h')
+    agg_rides = rides.groupby(['pickup_hour', 'pickup_location_id']).size().reset_index()
+    agg_rides.rename(columns = {0:'rides'}, inplace = True)
+
+    # add rows for (locations, pickup_hours) with 0 rides
+    agg_rides_all_slots = add_missing_slots(agg_rides)
+
+    return agg_rides_all_slots
